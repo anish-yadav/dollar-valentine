77ee799a091b569a39647740379e24e632dbde10
Anish Kumar Yadav <hney1000.ay1@gmail.com>
2021-01-28 13:42:53 +0530

    Progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
-
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import theme from '../theme'
 import { AppProps } from 'next/app'
+
+Router.events.on("routeChangeStart",() => NProgress.start());
+Router.events.on("routeChangeComplete",() => NProgress.done());
+Router.events.on("routeChangeError",() => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
